875802d8c509c7e43bcd8fca482af70441ac36b7
EricQyhan <Ericqyhan@gmail.com>
2022-04-19 21:51:14 -0400

    got code working with Kennedy and Luiz

--- js/main.js
+++ js/main.js
@@ -19,21 +19,33 @@ function getFetch() {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
-      // let val1 = Number(cardValue(data.cards[0].value));
-      // let val2 = Number(cardValue(data.cards[1].value));
       document.querySelector("#player1-card1").src = data.cards[0].image;
       document.querySelector("#player1-card2").src = data.cards[2].image;
       document.querySelector("#player2-Card1").src = data.cards[1].image;
       document.querySelector("#player2-Card2").src = data.cards[3].image;
+      player2Value();
       document.querySelector(".hit-me").addEventListener("click", bjHit);
 
-      let i = 4;
+      function player2Value() {
+        let val1 = Number(cardValue(data.cards[1].value));
+        let val2 = Number(cardValue(data.cards[3].value));
+        let startVal = val1 + val2;
+        document.querySelector(".player-2-total").innerText = startVal;
+      }
+
+      let i = 3;
       function bjHit() {
+        i += 1;
         const image = document.createElement("img");
         image.src = data.cards[i].image;
-        i += 1;
-        console.log(image.src);
         document.querySelector(".player2-cards-hit-me").appendChild(image);
+        //   document.querySelector(".player-2-total").innerText += Number(
+        //   cardValue(data.cards[i].value);
+        //   );
+        let x = +document.querySelector(".player-2-total").innerText;
+        x += +cardValue(data.cards[i].value);
+        document.querySelector(".player-2-total").innerText = x;
+        console.log(x);
       }
     })
 
